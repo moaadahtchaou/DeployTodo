4588be0dbbd184bc9b81f60a1f0cbd74ef2059fb
ZarakI <66420863+moaadahtchaou@users.noreply.github.com>
2024-01-14 16:34:43 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
+const mongoose=require("mongoose")
+const workout =require("./routes/workout")
 const express=require("express")
 const app =express()
 
 
+app.use(express.json())
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
 // Routes
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+app.use("/api/workout",workout)
 
+//Mongoose
 app.listen(3500,()=>{
         console.log("connect to db and listen",3500)
     })
+
+
 
     
