7c6b3d8dfbd14a81fa9fcc71ce05d7f5aaa5a91c
ZarakI <66420863+moaadahtchaou@users.noreply.github.com>
2024-01-14 16:33:44 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,14 @@
-const mongoose=require("mongoose")
-const workout =require("./routes/workout")
 const express=require("express")
 const app =express()
 
 
-app.use(express.json())
-
-app.use((req,res,next)=>{
-    console.log(req.path,req.method)
-    next()
-})
 // Routes
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-app.use("/api/workout",workout)
 
-//Mongoose
-mongoose.connect(process.env.MONG_URL)
-    .then(()=>{
-        //listen to req
-        app.listen(3500,()=>{
+app.listen(3500,()=>{
         console.log("connect to db and listen",3500)
-    })})
-    .catch((err)=>console.log(err))
+    })
+
     
